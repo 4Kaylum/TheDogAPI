d91000f55b9c4c99f4cf64cdd4119246fdaf7a89
Callum Bartlett <coolum444@hotmail.co.uk>
2017-07-02 21:09:09 +0100

    Submit page
    You can now submit a dog via AJAX on the actual site page.

--- v1/uiHandling.py
+++ v1/uiHandling.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 from .Utils.AllUtils import getDatabseVariable
-from .Utils.DatabaseQueries import getRandomVerifiedDogFromDatabase
+from .Utils.DatabaseQueries import getRandomVerifiedDogFromDatabase, getSpecificDogFromDatabase
 from .Utils.JsonReturnData import databaseQueryToDict
 
 
@@ -19,3 +19,18 @@ def doggoPage():
     dogID = dogData['data'][0]['id']
     dogURL = dogData['data'][0]['url']
     return render_template('dog.html', dogID=dogID, dogURL=dogURL)
+
+
+@ui_v1.route('/v1/dog/<dogPageID>')
+def doggoPageByID(dogPageID):
+    database = getDatabseVariable()
+    dogThing = getSpecificDogFromDatabase(database, dogPageID)
+    dogData = databaseQueryToDict(dogThing)
+    dogID = dogData['data'][0]['id']
+    dogURL = dogData['data'][0]['url']
+    return render_template('dog.html', dogID=dogID, dogURL=dogURL)
+
+
+@ui_v1.route('/v1/submit')
+def submitPage():
+    return render_template('submit.html')
