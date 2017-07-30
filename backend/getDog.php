72f96de1b8b116486c11a70eceb21ae1ed75d89c
Callum Bartlett <coolum444@hotmail.co.uk>
2017-07-30 02:22:47 +0100

    Added SQL function
    This is to make it so I don't need to reconnect to the database every
    time in every function. I can just run the function.

--- backend/getDog.php
+++ backend/getDog.php
@@ -1,29 +1,13 @@
 <?php
 
-   require('dogObject.php');
+   include('dogObject.php');
+   include('databaseHandling.php');
+
 
    function getRandomDog() {
-      // $hostDetails = parse_ini_file('./mysqlPassword.ini');
-      // $hostname = $hostDetails[0];
-      // $username = $hostDetails[1];
-      // $password = $hostDetails[2];
-
-      // $connection = mysql_connect($hostname, $username, $password);
-
-      // if(! $connection ) {
-      //    die('Could not connect: ' . mysql_error());
-      // }
 
       // $sql = 'SELECT * FROM DogPictures WHERE verified=1 ORDER BY RAND() LIMIT 1;';
-      // mysql_select_db('DogPictures');
-      // $values = mysql_query($sql, $connection);
-
-      // if(!$values) {
-      //    die('Could not get data: ' . mysql_error());
-      // }
-
-      // mysql_close($connection);
-      // mysql_free_result($values);
+      // $values = runSQL($sql);
 
       $values = Array('-2nGCLJMAN9', 'https://i.redd.it/kfm062pie0bz.jpg', '2017-07-22T04:16:42.474949', 'jpg', '0.0.0.0', 1, 1);
       $dogItem = new Dog($values);
@@ -32,28 +16,10 @@
    }
 
    function getSpecificDog($dogID) {
-      // $hostDetails = parse_ini_file('./mysqlPassword.ini');
-      // $hostname = $hostDetails[0];
-      // $username = $hostDetails[1];
-      // $password = $hostDetails[2];
-
-      // $connection = mysql_connect($hostname, $username, $password);
-
-      // if(! $connection ) {
-      //    die('Could not connect: ' . mysql_error());
-      // }
 
       // $escaped = mysql_real_escape_string($dogID);
       // $sql = 'SELECT * FROM DogPictures WHERE id=' . $escaped . ';';
-      // mysql_select_db('DogPictures');
-      // $values = mysql_query($sql, $connection);
-
-      // if(!$values) {
-      //    die('Could not get data: ' . mysql_error());
-      // }
-
-      // mysql_close($connection);
-      // mysql_free_result($values);
+      // $values = runSQL($sql);  
 
       $values = Array('-2nGCLJMAN9', 'https://i.redd.it/kfm062pie0bz.jpg', '2017-07-22T04:16:42.474949', 'jpg', '0.0.0.0', 1, 1);
       $dogItem = new Dog($values);
